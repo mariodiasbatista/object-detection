60366e7e7e00f30c25d15d833c85e5ad67d2f0ae
Mário Baptista <33545600+mariodiasbatista@users.noreply.github.com>
2025-03-21 01:15:48 +0000

    added color to output

--- test_detection.sh
+++ test_detection.sh
@@ -1,4 +1,3 @@
-  GNU nano 6.2                                  ./test_detection.sh *
 #!/bin/bash
 
 #for this test we need to load some pre-trained weights the objective is to test if darknet is correctly comp>
@@ -8,16 +7,20 @@ wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optim
 
 output=$(./darknet detect cfg/yolov4.cfg yolov4.weights data/person.jpg)
 
+GREEN='\e[32m'
+RED='\e[31m'
+NC='\e[0m' # Reset color
+
 if [[ "$output" == *"dog"* && "$output" == *"person"* &&  "$output" == *"horse"* ]]; then
-    echo -e "\n ${GREEN} ^|^e Detection is working!"
+    echo -e "\n${GREEN} Detection is working!${NC}"
 else
-    echo -e "\n ${RED} ^}^l Not Detecting!"
+    echo -e "\n${RED} Not Detecting!${NC}"
 fi
 
 filepath=$(find . -name predictions.jpg | head -n 1)
 
 if [[ -n "$filepath" ]]; then
-    echo -e "${GREEN} ^|^e Predictions generated at: $filepath \n"
+    echo -e "${GREEN} Predictions generated at: $filepath \n${NC}"
 else
-    echo -e "${RED} ^}^l Prediction file not found.\n"
+    echo -e "${RED} Prediction file not found. \n${NC}"
 fi
