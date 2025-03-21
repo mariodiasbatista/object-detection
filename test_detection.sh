0a08507d310580dba8fce434a892ef3bb0399f54
Mário Baptista <33545600+mariodiasbatista@users.noreply.github.com>
2025-03-21 01:07:26 +0000

    Update test_detection.sh

--- test_detection.sh
+++ test_detection.sh
@@ -1,22 +1,23 @@
+  GNU nano 6.2                                  ./test_detection.sh *
 #!/bin/bash
 
-#for this test we need to load some pre-trained weights the objective is to test if darknet is correctly compiled.
+#for this test we need to load some pre-trained weights the objective is to test if darknet is correctly comp>
 cd darknet
 ls -la
 wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
 
-output=$(./darknet detect cfg/yolov4.cfg yolov4.weights data/person.jpg) 
+output=$(./darknet detect cfg/yolov4.cfg yolov4.weights data/person.jpg)
 
 if [[ "$output" == *"dog"* && "$output" == *"person"* &&  "$output" == *"horse"* ]]; then
-    echo "Detection is working!"
+    echo -e "\n ${GREEN} ^|^e Detection is working!"
 else
-    echo "Not Detecting!"
+    echo -e "\n ${RED} ^}^l Not Detecting!"
 fi
 
 filepath=$(find . -name predictions.jpg | head -n 1)
 
 if [[ -n "$filepath" ]]; then
-    echo "Predictions generated at: $filepath"
+    echo -e "${GREEN} ^|^e Predictions generated at: $filepath \n"
 else
-    echo "Prediction file not found."
+    echo -e "${RED} ^}^l Prediction file not found.\n"
 fi
