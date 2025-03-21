87560d856c539c89d3eafdbc4412499397271b31
Mário Baptista <33545600+mariodiasbatista@users.noreply.github.com>
2025-03-21 00:48:31 +0000

    Create test_detection.sh
    Use this file to test if darknet framework is correctly compiled

---
+++ test_detection.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#for this test we need to load some pre-trained weights the objective is to test if darknet is correctly compiled.
+cd darknet
+ls -la
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+
+output=$(./darknet detect cfg/yolov4.cfg yolov4.weights data/person.jpg) 
+
+if [[ "$output" == *"dog"* && "$output" == *"person"* &&  "$output" == *"horse"*]]; then
+    echo "Detection is working!"
+else
+    echo "Not Detecting!"
+fi
+
+filepath=$(find . -name predictions.jpg | head -n 1)
+if filepath | grep -q .; then
+    echo "Predictions generated at: $filepath"
+else
+    echo "Prediction File not found."
+fi
